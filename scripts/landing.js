aa717b084e42576a86c78e9161776fe8055794b2
Bailey <bailey.g.weathers@gmail.com>
2016-11-09 13:44:58 -0500

    Complete checkpoint 23 DOM events

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,8 +1,8 @@
 
-var animatePoints = function() {
-
- var points = document.getElementsByClassName('point');
-
+var pointsArray = document.getElementsByClassName('point');
+ 
+ var animatePoints = function(points) {
+     
  var revealPoint = function(itemNo) {
      points[itemNo].style.opacity = 1;
      points[itemNo].style.transform = "scaleX(1) translateY(0)";
@@ -13,6 +13,16 @@ var animatePoints = function() {
 for (var itemNo = 0; itemNo < points.length; itemNo++) {
     revealPoint(itemNo);
 }
-
      
 };
+
+window.onload = function() {
+    var sellingPoints = document.getElementsByClassName('selling-points')[0];
+    var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+
+    window.addEventListener('scroll', function(event) {
+       if (document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
+             animatePoints(pointsArray);   
+         }
+     });
+}
