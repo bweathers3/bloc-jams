2d3d8690ca283f0a71cb646de4af743f51df43b2
Bailey <bailey.g.weathers@gmail.com>
2016-11-09 10:07:29 -0500

    Completed assignment 22 DOM aniamtion

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,31 +1,18 @@
 
 var animatePoints = function() {
 
-     var points = document.getElementsByClassName('point');
+ var points = document.getElementsByClassName('point');
 
-     var revealFirstPoint = function() {
-         points[0].style.opacity = 1;
-         points[0].style.transform = "scaleX(1) translateY(0)";
-         points[0].style.msTransform = "scaleX(1) translateY(0)";
-         points[0].style.WebkitTransform = "scaleX(1) translateY(0)";
-     };
+ var revealPoint = function(itemNo) {
+     points[itemNo].style.opacity = 1;
+     points[itemNo].style.transform = "scaleX(1) translateY(0)";
+     points[itemNo].style.msTransform = "scaleX(1) translateY(0)";
+     points[itemNo].style.WebkitTransform = "scaleX(1) translateY(0)";
+ };
 
-     var revealSecondPoint = function() {
-         points[1].style.opacity = 1;
-         points[1].style.transform = "scaleX(1) translateY(0)";
-         points[1].style.msTransform = "scaleX(1) translateY(0)";
-         points[1].style.WebkitTransform = "scaleX(1) translateY(0)";
-     };
+for (var itemNo = 0; itemNo < points.length; itemNo++) {
+    revealPoint(itemNo);
+}
 
-     var revealThirdPoint = function() {
-         points[2].style.opacity = 1;
-         points[2].style.transform = "scaleX(1) translateY(0)";
-         points[2].style.msTransform = "scaleX(1) translateY(0)";
-         points[2].style.WebkitTransform = "scaleX(1) translateY(0)";
-     };
-
-     revealFirstPoint();
-     revealSecondPoint();
-     revealThirdPoint();
-
+     
 };
