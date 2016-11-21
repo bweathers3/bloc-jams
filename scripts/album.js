c1234d0a687b43626fed5b60eb7a9fcf689421c5
Bailey <bailey.g.weathers@gmail.com>
2016-11-21 09:23:06 -0500

    Complete assignmanet 27 play and pause part 2

--- scripts/album.js
+++ scripts/album.js
@@ -68,13 +68,23 @@ var setCurrentAlbum = function(album) {
  };
 
 var findParentByClassName = function(element, targetClass) {
-    if (element) {
+    
+    if (element.parentElement === null){
+        console.log("No parent found");
+        
+    } else if (element.parentElement.className === null){
+        console.log("No parent found with that class name");
+        
+    } else{
+        
+        if (element) {
         var currentParent = element.parentElement;
         while (currentParent.className != targetClass && currentParent.className !== null) {
             currentParent = currentParent.parentElement;
         }
         return currentParent;
-    }
+     }
+   }
 };
 
 var getSongItem = function(element) {
